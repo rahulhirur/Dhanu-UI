c2baac3849119c08d205db60105fc15ab4e004e2
Pawan-Kumar-S <122091853+Pawan-Kumar-S@users.noreply.github.com>
2026-01-23 19:26:44 +0100

    Update gesture_example_symbol.py
    updated the gesture symbols

--- gesture_example_symbol.py
+++ gesture_example_symbol.py
@@ -1,1 +1,5 @@
 # This model can recognize 7 hand gestures: 👍, 👎, ✌️, ☝️, ✊, 👋, 🤟
+👍 - Bring me the Cathedral      #thumbs up
+✊ - stop the task               #closed hand
+👋 - come to the nutral spot    #open palm
+👎 - Disposal the used cotton   #thumbs down
